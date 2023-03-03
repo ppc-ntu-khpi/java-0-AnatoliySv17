d93d7565a37f953345d56b5b32d88ed65df6fdb3
AnatoliySv17 <120326304+AnatoliySv17@users.noreply.github.com>
2023-03-03 19:04:07 +0100

    Add files via upload

---
+++ src/Quotation.java
@@ -0,0 +1,7 @@
+public class Quotation {
+  String quote = "Це перш за все: бути правдивим перед самим собою";
+  String autor ="-Вільям Шекспір";
+  public void display() {
+    System.out.println(quote);
+    System.out.println(autor);
+  }
